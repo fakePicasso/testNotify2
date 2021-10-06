513d10a7c17bbe1a78fb79b809df0ba0d2e0d51b
contactatfp <85082344+contactatfp@users.noreply.github.com>
2021-10-06 11:58:48 -0700

    post version failure. login fixed

--- testNotify2/LoginViewController.swift
+++ testNotify2/LoginViewController.swift
@@ -12,6 +12,8 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var LoginEmail: UITextField!
     @IBOutlet weak var LoginPass: UITextField!
     
+    var loginToken:String?
+    
     public var completionHandler: ((String?)-> Void)?
 
     override func viewDidLoad() {
@@ -26,7 +28,16 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
         // Do any additional setup after loading the view.
     }
     
-    
+    func shakeAnimation() {
+            let animation = CABasicAnimation(keyPath: "position")
+            animation.duration = 0.2
+            animation.repeatCount = 2
+            animation.speed = 5
+            animation.autoreverses = true
+            animation.fromValue = NSValue(cgPoint: CGPoint(x: self.view.center.x - 10, y:  self.view.center.y))
+            animation.toValue = NSValue(cgPoint: CGPoint(x: self.view.center.x + 10, y:  self.view.center.y))
+            self.view.layer.add(animation, forKey: "position")
+        }
     
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
@@ -38,50 +49,156 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
             }
             return false
         }
+    @IBAction func loginBtn(_ sender: Any)
+        {
+            
+            let token = "99479211236bf2cb4823f1330ce8fa7a"
+        //
+                let authValue: String? = "Bearer \(token)"
 
+                let loginUrl = "https://schoolnotifier.bubbleapps.io/version-test/api/1.1/wf/login_copy2"
+                let parameters = ["email": LoginEmail.text ?? "" , "pass" : LoginPass.text ?? ""]
+                let headers : HTTPHeaders  = ["Authorization": authValue ?? ""]
+                
+                AF.request(loginUrl,
+                           method: .post,
+                           parameters: parameters,
+                           encoder: URLEncodedFormParameterEncoder.default,
+                           headers: headers,
+                           interceptor: nil).responseJSON{  [weak self] response in
+                    
+                    print("***************",response.result)
+
+                    switch response.result {
+                                    case .success:
+                                        if let data = response.data {
+                                            print(data)
+                                            // Convert This in JSON
+                                            do {
+                                                let responseDecoded = try JSONDecoder().decode(APIResponse.self, from: data)
+                                                print("%%%%%%%%%%%%%%%%%%%%")
+                                                print("USER: HELP", responseDecoded.response.token)
+                                                self?.loginToken = responseDecoded.response.token
+                                                print(self?.loginToken ?? "")
+                                                dateComponents.year = 1
+                                                dateComponents.day = -1
+                                                loginTokenGlobal = self?.loginToken
+                                                expiration = Calendar.current.date(byAdding: dateComponents, to: currentDateAndTime)
+                                                
+
+                                            }catch let error as NSError{
+                                                print(error)
+                                            }
+                                        }
+                                    case .failure(let error):
+                                        print("Error:", error)
+                                    }
+                            
+        //                    if string.value?.contains("success") ?? false
+                            if ((self?.loginToken) != nil){
+                                DispatchQueue.main.async {
+                                    UserDefaults.standard.set(true, forKey: "didLogin")
+                     
+                                    let storyboard : UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+                                    let tabbarVC = storyboard.instantiateViewController(withIdentifier: "TabbarVC") as! UITabBarController
+                                    guard let vc = tabbarVC.viewControllers![0] as? ViewController else {return}
+                                    vc.authToken = self?.LoginEmail.text!
+                                    vc.passToken = self?.LoginPass.text!
+                                    //vc.loginToken2 = self?.loginToken
+                                    UIApplication.shared.windows[0].rootViewController = tabbarVC
+                  
+                                }
+                            }
+                            if ((self?.loginToken) == nil){
+                                self?.shakeAnimation()
+                                self?.LoginEmail.text = ""
+                                self?.LoginPass.text = ""
+                            }
+               
+                }
+            }
+    
+    
     
     //@IBAction func btnLoginPressed(_ sender: Any) {
-    @IBAction func btnLoginPressed(_ sender: Any, forEvent event: UIEvent) {
-
-        //dismiss(animated: true, completion: nil)
-        let token = "99479211236bf2cb4823f1330ce8fa7a"
-        let authValue: String? = "Bearer \(token)"
-        let loginUrl = "https://schoolnotifier.bubbleapps.io/version-test/api/1.1/wf/login_copy"
-        let parameters = ["email": LoginEmail.text ?? "" , "pass" : LoginPass.text ?? ""]
-        let headers : HTTPHeaders  = ["Authorization": authValue ?? ""]
-
-        completionHandler?(LoginEmail.text)
-
-        // All three of these calls are equivalent
-        AF.request(loginUrl, method: .post, parameters: parameters, encoder: URLEncodedFormParameterEncoder.default, headers: headers, interceptor: nil).responseString { (string) in
-            
-            if string.value?.contains("success") ?? false{
-                
-                DispatchQueue.main.async {
-                    UserDefaults.standard.set(true, forKey: "didLogin")
-                    
-                    let storyboard : UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
-                    let vc = storyboard.instantiateViewController(withIdentifier: "TabbarVC") as! UITabBarController
-                    UIApplication.shared.windows[0].rootViewController = vc
-  
-                }
-                
-            }
-            
-        }
-        
-    }
+//    @IBAction func btnLoginPressed(_ sender: Any, forEvent event: UIEvent) {
+//
+//        let token = "99479211236bf2cb4823f1330ce8fa7a"
+//    //
+//            let authValue: String? = "Bearer \(token)"
+//
+//            let loginUrl = "https://schoolnotifier.bubbleapps.io/version-test/api/1.1/wf/login_copy2"
+//            let parameters = ["email": LoginEmail.text ?? "" , "pass" : LoginPass.text ?? ""]
+//            let headers : HTTPHeaders  = ["Authorization": authValue ?? ""]
+//
+//            AF.request(loginUrl,
+//                       method: .post,
+//                       parameters: parameters,
+//                       encoder: URLEncodedFormParameterEncoder.default,
+//                       headers: headers,
+//                       interceptor: nil).responseJSON{  [weak self] response in
+//
+//                print("***************",response.result)
+//
+//                switch response.result {
+//                                case .success:
+//                                    if let data = response.data {
+//                                        print(data)
+//                                        // Convert This in JSON
+//                                        do {
+//                                            let responseDecoded = try JSONDecoder().decode(APIResponse.self, from: data)
+//                                            print("%%%%%%%%%%%%%%%%%%%%")
+//                                            print("USER: HELP", responseDecoded.response.token)
+//                                            self?.loginToken = responseDecoded.response.token
+//                                            print(self?.loginToken ?? "")
+//                                            dateComponents.year = 1
+//                                            dateComponents.day = -1
+//                                            loginTokenGlobal = self?.loginToken
+//                                            expiration = Calendar.current.date(byAdding: dateComponents, to: currentDateAndTime)
+//
+//
+//                                        }catch let error as NSError{
+//                                            print(error)
+//                                        }
+//                                    }
+//                                case .failure(let error):
+//                                    print("Error:", error)
+//                                }
+//
+//    //                    if string.value?.contains("success") ?? false
+//                        if ((self?.loginToken) != nil){
+//                            DispatchQueue.main.async {
+//                                UserDefaults.standard.set(true, forKey: "didLogin")
+//
+//                                let storyboard : UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+//                                let tabbarVC = storyboard.instantiateViewController(withIdentifier: "TabbarVC") as! UITabBarController
+//                                guard let vc = tabbarVC.viewControllers![0] as? ViewController else {return}
+//                                vc.authToken = self?.LoginEmail.text!
+//                                vc.passToken = self?.LoginPass.text!
+//                                //vc.loginToken2 = self?.loginToken
+//                                UIApplication.shared.windows[0].rootViewController = tabbarVC
+//
+//                            }
+//                        }
+//                        if ((self?.loginToken) == nil){
+//                            self?.shakeAnimation()
+//                            self?.LoginEmail.text = ""
+//                            self?.LoginPass.text = ""
+//                        }
+//
+//            }
+//        }
     //*****
-    @IBAction func testBtn(_ sender: Any, forEvent event: UIEvent) {
-        
-        let vc = storyboard?.instantiateViewController(identifier: "other") as! ViewController
-        //vc.modalPresentationStyle = .fullScreen
-        
-        present(vc, animated: true)
-        completionHandler?(LoginEmail.text)
-        
-        
-        //dismiss(animated: true, completion: nil)
-    }
+//    @IBAction func testBtn(_ sender: Any, forEvent event: UIEvent) {
+//
+//        let vc = storyboard?.instantiateViewController(identifier: "other") as! ViewController
+//        //vc.modalPresentationStyle = .fullScreen
+//
+//        present(vc, animated: true)
+//        completionHandler?(LoginEmail.text)
+//
+//
+//        //dismiss(animated: true, completion: nil)
+//    }
     
 }
